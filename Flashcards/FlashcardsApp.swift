669df21ce5dff534a7605d65457f07af8b47f97b
Caleb Roland <calebproland@gmail.com>
2022-10-06 12:11:37 -0400

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Slave I on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
